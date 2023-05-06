e4df411e15f55bd90c877c1b4a4b81d6e77d9065
TERMINATOR <TERMINATOR@DESKTOP-UKAHK9I>
2023-05-06 12:24:33 -0500

    ¡Se sube cambios!

--- src/main/java/co/empresa/adulam/controller/ClienteController.java
+++ src/main/java/co/empresa/adulam/controller/ClienteController.java
@@ -26,116 +26,130 @@ import co.empresa.adulam.services.ProductoService;
 @Controller
 @RequestMapping("/adulamstore")
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
-    @Autowired
-    private CategoriaService categoriaService;
-	
+
+	@Autowired
+	private CategoriaService categoriaService;
+
 	List<Producto> listMostrar = new ArrayList<>();
 
-	
 	@GetMapping("")
-	public String store(Model model) {
+	public String store(HttpServletRequest request, Model model) {
 		List<Categoria> listCategoria = categoriaService.getAll();
+		if(request.getSession().getAttribute("cliente_id") != null) {
+			int c_id = (int)request.getSession().getAttribute("cliente_id");
+			Cliente cli = clienteService.get(c_id);
+			model.addAttribute("cliente", cli.getNombre());
+			model.addAttribute("categoria", listCategoria);
+			return "home";
+		}
 		model.addAttribute("categoria", listCategoria);
 		return "home";
 	}
-	
+
 	@GetMapping("/login")
 	public String login(HttpServletRequest request, HttpSession session, Model model) {
-		if(request.getSession().getAttribute("cliente_id") != null) {
+		if (request.getSession().getAttribute("cliente_id") != null) {
 			return "redirect:/adulamstore/list";
 		}
 		return "login";
 	}
-	
+
 	@PostMapping("/singin")
-	public String validate(RedirectAttributes att, @RequestParam String email, @RequestParam String password, 
-							HttpServletRequest request, HttpSession session,  Model model) {
+	public String validate(RedirectAttributes att, @RequestParam String email, @RequestParam String password,
+			HttpServletRequest request, HttpSession session, Model model) {
 		Cliente cliente = clienteService.select(email, password);
-		
-		if(cliente != null)
-		{
+
+		if (cliente != null) {
 			request.getSession().setAttribute("cliente_id", cliente.getId());
 			request.getSession().setAttribute("cliente_nombre", cliente.getNombre());
 			model.addAttribute("cliente", cliente);
 			att.addFlashAttribute("cliente", cliente.getNombre());
 			return "redirect:/adulamstore/list";
-		}else {
+		} else {
 			att.addFlashAttribute("loginError", "Usuario o contraseña incorrecta");
 			return "redirect:/adulamstore/login";
-			}
+		}
 	}
-	
+
 	@GetMapping("/logout")
-	public String logout(HttpServletRequest request, HttpSession session,  Model model) {
-			request.getSession().invalidate();
-			return "redirect:/adulamstore";
+	public String logout(HttpServletRequest request, HttpSession session, Model model) {
+		request.getSession().invalidate();
+		return "redirect:/adulamstore";
 	}
-	
+
 	@PostMapping("/save")
 	public String insertClient(RedirectAttributes att, Cliente cliente, Model model) {
 		clienteService.save(cliente);
 		att.addFlashAttribute("accion", "¡Te has registrado con éxito!");
 		return "redirect:/adulamstore/login";
 	}
-	
+
 	@GetMapping("/edit/{id}")
-	public String editClient(HttpServletRequest request, HttpSession session,
-							@PathVariable("id") Integer id, Cliente cliente, Model model) {
-		if(id!=null) {
+	public String editClient(HttpServletRequest request, HttpSession session, @PathVariable("id") Integer id,
+			Cliente cliente, Model model) {
+		if (id != null) {
 			model.addAttribute("cliente", clienteService.get(id));
-		}else {
+		} else {
 			model.addAttribute("cliente", new Cliente());
 		}
 		return "editcliente";
 	}
-	
+
 	@GetMapping("/list/{id}")
-	public String listProductId(HttpServletRequest request, HttpSession session,
-								@PathVariable("id") Integer id,RedirectAttributes att, Model model) {
+	public String listProductId(HttpServletRequest request, HttpSession session, @PathVariable("id") Integer id,
+			RedirectAttributes att, Model model) {
 		List<Categoria> listCategoria = categoriaService.getAll();
 		model.addAttribute("categoria", listCategoria);
 		List<Producto> listProductos = productoService.getAll();
-		if(request.getSession().getAttribute("cliente_id") != null) {
-			int c_id = (int)request.getSession().getAttribute("cliente_id");
+		if (request.getSession().getAttribute("cliente_id") != null) {
+			int c_id = (int) request.getSession().getAttribute("cliente_id");
 			Cliente cli = clienteService.get(c_id);
+			listMostrar.clear();
+			listProductos.forEach((producto) -> {
+				if (producto.getId_categoria() == id)
+					listMostrar.add(producto);
+			});
 			model.addAttribute("cliente", cli.getNombre());
+			model.addAttribute("listProducto", listMostrar);
+			model.addAttribute("categoria", listCategoria);
 			return "adulamstore";
 		}
 		listMostrar.clear();
-		listProductos.forEach((producto)->{
-			if(producto.getId_categoria() == id)
+		listProductos.forEach((producto) -> {
+			if (producto.getId_categoria() == id)
 				listMostrar.add(producto);
 		});
 		model.addAttribute("listProducto", listMostrar);
 		return "adulamstore";
 	}
-	
+
 	@GetMapping("/list")
 	public String listProducto(HttpServletRequest request, HttpSession session, Model model) {
 		List<Producto> listProductos = productoService.getAll();
 		List<Categoria> listCategoria = categoriaService.getAll();
 		model.addAttribute("categoria", listCategoria);
 		model.addAttribute("listProducto", listProductos);
-		if(request.getSession().getAttribute("cliente_id") != null) {
-			int c_id = (int)request.getSession().getAttribute("cliente_id");
+		if (request.getSession().getAttribute("cliente_id") != null) {
+			int c_id = (int) request.getSession().getAttribute("cliente_id");
 			Cliente cli = clienteService.get(c_id);
-			model.addAttribute("cliente", cli.getNombre());	
+			model.addAttribute("categoria", listCategoria);
+			model.addAttribute("listProducto", listProductos);
+			model.addAttribute("cliente", cli.getNombre());
 			return "adulamstore";
-			
-		}
-		else return "adulamstore";
+
+		} else
+			return "adulamstore";
 	}
-	
+
 	@GetMapping("/new")
 	public String showForm(Model model) {
 		return "registerpersona";
 	}
-	
+
 }
---
+++ src/main/resources/static/js2/script.js
@@ -0,0 +1,111 @@
+const deletePaciente = document.querySelectorAll('.delete');
+
+const confirmar = document.querySelector('#confirmar');
+
+const searchInput = document.getElementById('search-input');
+
+const table = document.getElementById('tablax');
+
+const tableRows = table.getElementsByTagName('tr');
+
+const tableHeader = table.getElementsByTagName('thead')[0];
+
+const headerRow = tableHeader.getElementsByTagName('tr')[0];
+
+searchInput.addEventListener('keyup', function() {
+
+const searchValue = this.value.toLowerCase();
+
+const searchResults = [];
+
+searchResults.push(headerRow);
+
+for (let i = 0; i < tableRows.length; i++) {
+
+const rowCells = tableRows[i].getElementsByTagName("td");
+
+let found = false;
+
+for (let j = 0; j < rowCells.length; j++) {
+
+const cellText = rowCells[j].textContent.toLowerCase();
+
+if (cellText.indexOf(searchValue) > -1) {
+
+found = true;
+
+break;
+
+}
+
+}
+
+if (found) {
+
+searchResults.push(tableRows[i]);
+
+}
+
+}
+
+for (let i = 0; i < tableRows.length; i++) {
+
+if (searchResults.indexOf(tableRows[i]) > -1) {
+
+tableRows[i].style.display = "";
+
+} else {
+
+tableRows[i].style.display = "none";
+
+}
+
+}
+
+});
+
+if(deletePaciente){
+
+deletePaciente.forEach(function(deletePaciente){
+
+deletePaciente.addEventListener('click', function(){
+
+var documento = this.closest('tr').querySelector('.documento');
+
+var numeroDocumento = documento.textContent;
+
+localStorage.setItem('documento', numeroDocumento);
+
+});
+
+});
+
+}
+
+if(confirmar){
+
+confirmar.addEventListener('click', function(){
+
+window.location.href = '/paciente/delete/'+localStorage.getItem('documento');
+
+});
+
+localStorage.clear();
+
+}
+
+function togglePassword() {
+
+var passwordInput = document.getElementById("password");
+
+if (passwordInput.type === "password") {
+
+passwordInput.type = "text";
+
+} else {
+
+passwordInput.type = "password";
+
+}
+
+}
